5f227f947378c64f901f5f8fbb9a3cadc255c83b
Nicholas <nickadamu@gmail.com>
2019-08-25 23:46:13 +0000

    Added filter for search

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,26 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { TaskStatus } from '../tasks.model';
+
+export class TaskValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
+  transform(value: any) {
+    value  = value.toUpperCase();
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`"${value} is an invalid status"`);
+    }
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
+  }
+}
