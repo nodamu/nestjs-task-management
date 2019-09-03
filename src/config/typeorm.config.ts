5170dd9b8ed09f1fbe6c0b0ff0e30f4fd5a045dd
Nicholas <nickadamu@gmail.com>
2019-09-03 02:18:04 +0000

    Added typeorm config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,11 @@
+import {TypeOrmModuleOptions} from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'osafo',
+    database: 'taskmanagement',
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: true,
+}
